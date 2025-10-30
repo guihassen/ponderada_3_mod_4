9fd7c70514cb8f95efbf10094c2f78bc39b3b179
guihassen <guilherme.hassenpflug@sou.inteli.edu.br>
2025-10-30 12:07:03 -0300

    parte-sala

---
+++ Parte 1/ponderada_3/ponderada_3.ino
@@ -0,0 +1,52 @@
+// === Definição dos pinos ===
+int ledVermelho = 8;
+int ledAmarelo  = 9;
+int ledVerde    = 7;   // alterado para porta 7
+int buzzer      = 11;  // buzzer na porta 11
+
+
+void acenderLED(int *pino) {
+  digitalWrite(*pino, HIGH);
+}
+
+void apagarLED(int *pino) {
+  digitalWrite(*pino, LOW);
+}
+
+
+void avisoBuzzer(int *pinoBuzzer) {
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(*pinoBuzzer, HIGH);
+    delay(200);
+    digitalWrite(*pinoBuzzer, LOW);
+    delay(300);
+  }
+}
+
+
+void setup() {
+  pinMode(ledVermelho, OUTPUT);
+  pinMode(ledAmarelo, OUTPUT);
+  pinMode(ledVerde, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+}
+
+
+void loop() {
+  
+  acenderLED(&ledVerde);
+  delay(4000);              // permanece 4s
+  avisoBuzzer(&buzzer);     // "bip, bip, bip"
+  apagarLED(&ledVerde);
+
+  
+  acenderLED(&ledAmarelo);
+  delay(2000);
+  apagarLED(&ledAmarelo);
+
+  
+  acenderLED(&ledVermelho);
+  delay(6000);
+  avisoBuzzer(&buzzer);
+  apagarLED(&ledVermelho);
+}
